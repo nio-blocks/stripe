80f43572b6b249165a2f85e57152ab5ac21fe977
cowleyk <cowley.kevin.l@gmail.com>
2017-11-14 11:45:41 -0700

    :white_check_mark: Add block unit tests

--- tests/test_stripe_block.py
+++ tests/test_stripe_block.py
@@ -1,19 +1,46 @@
-from nio.block.terminals import DEFAULT_TERMINAL
-from nio.signal.base import Signal
+import json
+from unittest.mock import MagicMock, patch
+
+import responses
+
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..stripe_block import Stripe
+from ..stripe_block import Stripe, BuildSignal
 
 
-class TestExample(NIOBlockTestCase):
+class TestBuildSignal(NIOBlockTestCase):
 
-    def test_process_signals(self):
-        """Signals pass through block unmodified."""
+    def test_web_handler_post_dict(self):
+        notify_signals = MagicMock()
+        handler = BuildSignal(endpoint='',
+                              notify_signals=notify_signals,
+                              logger=MagicMock())
+        request = MagicMock()
+        request._body.read.return_value.decode.return_value = '{"Im a": "dictionary"}'
+
+        with patch(BuildSignal.__module__ + '.stripe') as mock_stripe:
+            handler.on_post(request, MagicMock())
+            self.assertDictEqual(
+                notify_signals.call_args[0][0][0].to_dict(),
+                {"Im a": "dictionary"})
+
+
+class TestStripeBlock(NIOBlockTestCase):
+
+    # @responses.activate
+    def test_web_server_created(self):
         blk = Stripe()
-        self.configure_block(blk, {})
-        blk.start()
-        blk.process_signals([Signal({"hello": "nio"})])
-        blk.stop()
-        self.assert_num_signals_notified(1)
-        self.assertDictEqual(
-            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "nio"})
+
+        with patch(Stripe.__module__ + '.WebEngine') as mock_engine:
+            with patch(Stripe.__module__ + '.BuildSignal') as mock_handler:
+                self.configure_block(blk, {})
+                mock_engine.add_server.assert_called_once_with(
+                    blk.web_server().port(),
+                    blk.web_server().host(),
+                )
+                mock_handler.assert_called_once_with(
+                    blk.web_server().endpoint(),
+                    blk.notify_signals,
+                    blk.logger,
+                )
+            # self.assertEqual(len(responses.calls), 0)
+
